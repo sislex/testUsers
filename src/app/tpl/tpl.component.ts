3967e9742c26d6d5fa77b838eda1afb74c0202f7
Alex Rozhnov <alex.rozhnov@capsilon.com>
2017-07-05 22:18:57 +0300

    start

---
+++ src/app/tpl/tpl.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tpl',
+  templateUrl: './tpl.component.html',
+  styleUrls: ['./tpl.component.less']
+})
+export class TplComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
